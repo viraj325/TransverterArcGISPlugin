a37baf721da11671ac9e4a21a12fa8aef3bbdffd
Viraj Patel <37918393+viraj325@users.noreply.github.com>
2022-04-24 18:59:45 -0400

    Add files via upload

---
+++ ToolValidator.py
@@ -0,0 +1,34 @@
+﻿class ToolValidator:
+    # Class to add custom behavior and properties to the tool and tool parameters.
+
+    def __init__(self):
+        # set self.params for use in other function
+        self.params = arcpy.GetParameterInfo()
+
+    def initializeParameters(self):
+        # Customize parameter properties. 
+        # This gets called when the tool is opened.
+        return
+
+    def updateParameters(self):
+        # Modify parameter values and properties.
+        # This gets called each time a parameter is modified, before 
+        # standard validation.
+        if self.params[5].value == True:
+            self.params[6].enabled = True
+            self.params[7].enabled = True
+            self.params[8].enabled = True
+        else:
+            self.params[6].enabled = False
+            self.params[7].enabled = False
+            self.params[8].enabled = False
+        return
+
+    def updateMessages(self):
+        # Customize messages for the parameters.
+        # This gets called after standard validation.
+        return
+
+    # def isLicensed(self):
+    #     # set tool isLicensed.
+    #     return True
